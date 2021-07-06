41c790bb8e8a6f19a8a07237f36b544a3ef7f3f8
Agus Setiadi <sydoels@gmail.com>
2021-07-06 23:55:33 +0700

    add config

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,9 @@
  * @package WordPress
  */
 
+define( 'WP_HOME', 'http://localhost:8001/wp-landing' );
+define( 'WP_SITEURL', 'http://localhost:8001/wp-landing' );
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'wp_landing' );
